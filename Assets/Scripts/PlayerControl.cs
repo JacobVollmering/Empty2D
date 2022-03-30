cc1fe854c404a912a35375faaf6ce06fd82bdf6b
ZachKSimpson <97473134+ZachKSimpson@users.noreply.github.com>
2022-03-30 01:58:58 -0500

    Changing PlayerMovement
    Rework of PlayerControl for future animations

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,54 +4,58 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    public float speed = 5;
+    Rigidbody2D rb;
+
+    float walkSpeed = 4f;
+    float speedLimiter = 0.7f;
+    float inputHorizontal;
+    float inputVertical;
+
+    //Animations and States
+    Animator animator;
+    string currentState;
+    const string PLAYER_IDLE = "Player_Idle";
+
     // Start is called before the first frame update
     void Start()
     {
-        oldpos = transform.position;   
+        rb = gameObject.GetComponent<Rigidbody2D>();
+
+
+
     }
 
-    Vector3 oldpos;
-    float encdist = 0;
-    float lastroll = -1;
-    public float maxencdist = 80;
-    public float minencdist = 5;
-    void OnTriggerStay2D(Collider2D collision)
-    {
-        EncounterTable et;
-        if(collision.gameObject.TryGetComponent(out et))
-        {
-            encdist += (transform.position - oldpos).magnitude;
-            if (encdist - lastroll > 1)
-            {
-                lastroll = encdist;
-                if (Random.value < (encdist - minencdist) / (maxencdist - minencdist))
-                {
-                    FindObjectOfType<GameManager>().BeginBattle(et.GetRandomEncounter());
-                    encdist = 0;
-                }
-            }
-        }
-    }
     // Update is called once per frame
     void Update()
     {
-        Rigidbody2D body = GetComponent<Rigidbody2D>();
-        body.velocity = Vector3.ClampMagnitude(
-               new Vector3(Input.GetAxis("Horizontal"),
-               Input.GetAxis("Vertical")),
-               1) * speed;
-        /*  transform.position +=
-               Vector3.ClampMagnitude(
-                 new Vector3(Input.GetAxis("Horizontal"),
-                 Input.GetAxis("Vertical")),
-                 1)*Time.deltaTime*speed;*/
+        //Movement Block
+        inputHorizontal = Input.GetAxisRaw("Horizontal");
+        inputVertical = Input.GetAxisRaw("Vertical");
     }
 
-
-
-    void LateUpdate()
+    void FixedUpdate()
     {
-        oldpos = transform.position;
+        if(inputHorizontal !=0 || inputVertical != 0)
+        {
+            
+            if(inputHorizontal != 0 && inputVertical != 0)
+            {
+                inputHorizontal *= speedLimiter;
+                inputVertical *= speedLimiter;
+
+            }
+            
+            rb.velocity = new Vector2(inputHorizontal*walkSpeed , inputVertical *walkSpeed);
+        }
+        else
+        {
+            rb.velocity = new Vector2(0f, 0f);
+        }
     }
+
+    void ChangeAnimationState(string newSate)
+    {
+
+    }
+
 }
