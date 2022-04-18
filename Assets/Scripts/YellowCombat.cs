9abc67f8797358819f047f4e38a1d369f4bc89c7
IanGWhite <97561180+IanGWhite@users.noreply.github.com>
2022-04-17 20:11:13 -0500

    Revert "Revert "Edited some things with colliders""
    This reverts commit b8dc5a3f240b5f1a64eba5edd212efdc8b757f80.

--- Assets/Scripts/YellowCombat.cs
+++ Assets/Scripts/YellowCombat.cs
@@ -1,31 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class LavaDamage : MonoBehaviour
+public class YellowCombat : MonoBehaviour
 {
     void OnTriggerStay2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Damaging")
+        if (collision.gameObject.tag == "Enemy")
         {
             FindObjectOfType<PlayerStats>().DoDamage(1);
             Debug.Log("Yeowch!");
-        }
-        if (collision.gameObject.tag == "Enemy")
-        {
-            SceneManager.LoadScene("Battle Scene");
         }
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
